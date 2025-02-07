8383e1f489d65065e7428f581b7aa61ca49e6ae3
ktotopawel <pawel.juszczak02@gmail.com>
2025-02-07 14:42:38 +0100

    implemented board rendering

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 function GameController(player1 = 1, player2 = 2) {
   let board = Gameboard();
 
+  const cell1 = document.getElementById("cell1");
+  const cell2 = document.getElementById("cell2");
+  const cell3 = document.getElementById("cell3");
+  const cell4 = document.getElementById("cell4");
+  const cell5 = document.getElementById("cell5");
+  const cell6 = document.getElementById("cell6");
+  const cell7 = document.getElementById("cell7");
+  const cell8 = document.getElementById("cell8");
+  const cell9 = document.getElementById("cell9");
+
+  let cellArray = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9];
+
   console.log(board.getCellValues());
 
   let activePlayer = player1;
 
+  let boardArray = board.getCellValues();
+
   function playRound(col, row) {
-    if (board.getCellValues()[col][row] === 0){
-        board.changeCellValue(activePlayer, col, row);
+    if (board.getCellValues()[col][row] === 0) {
+      board.changeCellValue(activePlayer, col, row);
     } else {
-        console.log('spot taken');
-        return;
+      console.log("spot taken");
+      return;
     }
     checkForWin();
+    renderBoard();
+
     newRound();
     console.log(board.getCellValues());
   }
@@ -32,28 +48,36 @@ function GameController(player1 = 1, player2 = 2) {
   }
 
   function checkForWin() {
-    let boardArray = board.getCellValues();
+    boardArray = board.getCellValues();
 
     console.log(`checkForWinFn`);
     console.log(boardArray);
 
     if (
       (boardArray[0][0] === boardArray[0][1] &&
-        boardArray[0][1] === boardArray[0][2]) && boardArray[0][0] !== 0 ||
+        boardArray[0][1] === boardArray[0][2] &&
+        boardArray[0][0] !== 0) ||
       (boardArray[1][0] === boardArray[1][1] &&
-        boardArray[1][1] === boardArray[1][2]) && boardArray[1][1] !== 0  ||
+        boardArray[1][1] === boardArray[1][2] &&
+        boardArray[1][1] !== 0) ||
       (boardArray[2][0] === boardArray[2][1] &&
-        boardArray[2][1] === boardArray[2][2]) && boardArray[2][1] !== 0  ||
+        boardArray[2][1] === boardArray[2][2] &&
+        boardArray[2][1] !== 0) ||
       (boardArray[0][0] === boardArray[1][0] &&
-        boardArray[1][0] === boardArray[2][0]) && boardArray[1][0] !== 0  ||
+        boardArray[1][0] === boardArray[2][0] &&
+        boardArray[1][0] !== 0) ||
       (boardArray[0][1] === boardArray[1][1] &&
-        boardArray[1][1] === boardArray[2][1]) && boardArray[1][1] !== 0  ||
+        boardArray[1][1] === boardArray[2][1] &&
+        boardArray[1][1] !== 0) ||
       (boardArray[0][2] === boardArray[1][2] &&
-        boardArray[1][2] === boardArray[2][2]) && boardArray[1][2] !== 0  ||
+        boardArray[1][2] === boardArray[2][2] &&
+        boardArray[1][2] !== 0) ||
       (boardArray[0][0] === boardArray[1][1] &&
-        boardArray[1][1] === boardArray[2][2]) && boardArray[1][1] !== 0  ||
-      (boardArray[0][2] === boardArray[1][1] && 
-        boardArray[1][1] === boardArray[2][0]) && boardArray[1][1] !== 0
+        boardArray[1][1] === boardArray[2][2] &&
+        boardArray[1][1] !== 0) ||
+      (boardArray[0][2] === boardArray[1][1] &&
+        boardArray[1][1] === boardArray[2][0] &&
+        boardArray[1][1] !== 0)
     ) {
       console.log(`Game over ${activePlayer} wins`);
       resetGame();
@@ -68,9 +92,45 @@ function GameController(player1 = 1, player2 = 2) {
     activePlayer = player1;
   }
 
+  function renderBoard() {
+    let boardStateArray = boardArray.reduce((arr, row) => {
+        arr = arr.concat(row);
+        return arr;
+    }, [])
+
+    console.log(boardStateArray);
+
+    for (let i = 0; i < cellArray.length; i++) {
+        if (cellArray[i].firstChild) {
+            cellArray[i].removeChild(cellArray[i].firstChild);
+        }
+    }
+
+    for (let i = 0; i < boardStateArray.length; i++) {
+        switch (boardStateArray[i]) {
+            case 0: 
+                break;
+            case 1:
+                const x = document.createElement('img')
+                x.src = 'src/x.svg'
+                x.classList.add('icon');
+                cellArray[i].appendChild(x);
+                break;
+            case 2:
+                const o = document.createElement('img')
+                o.src = 'src/o.svg'
+                o.classList.add('icon');
+                cellArray[i].appendChild(o);
+                break;
+        }
+        
+    }
+  }
+
   return {
     resetGame,
-    playRound
+    playRound,
+    renderBoard
   };
 }
 
@@ -131,4 +191,6 @@ function Cell() {
 
 game = GameController();
 
-// gameboard = Gameboard();
+game.playRound(0, 0);
+game.playRound(1, 2);
+game.playRound(0, 1);
