4bf69cd567c8d56da0862ca43aa6df45a1308763
ktotopawel <pawel.juszczak02@gmail.com>
2025-02-07 17:01:48 +0100

    ability to change player names, a bit of code cleanup and finished project

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-const cell1 = document.getElementById("cell1");
-const cell2 = document.getElementById("cell2");
-const cell3 = document.getElementById("cell3");
-const cell4 = document.getElementById("cell4");
-const cell5 = document.getElementById("cell5");
-const cell6 = document.getElementById("cell6");
-const cell7 = document.getElementById("cell7");
-const cell8 = document.getElementById("cell8");
-const cell9 = document.getElementById("cell9");
-
-const cellBoard = document.querySelector(".gameboard");
-
-const startBtn = document.querySelector(".start");
-const resetBtn = document.querySelector(".reset");
-
-function GameController(player1 = 1, player2 = 2) {
+function GameController() {
   let board = Gameboard();
 
-  let cellArray = [
-    cell1,
-    cell2,
-    cell3,
-    cell4,
-    cell5,
-    cell6,
-    cell7,
-    cell8,
-    cell9,
-  ];
+  let player1 = {
+    i: 1,
+    playerDisplay: document.querySelector(".p1"),
+  };
+
+  let player2 = {
+    i: 2,
+    playerDisplay: document.querySelector(".p2"),
+  };
+
+  const result = document.querySelector(".result");
+
+  const p1Counter = document.querySelector(".p1-score");
+  const p2Counter = document.querySelector(".p2-score");
+
+  const cellArray = DomHandler().gameboardHandler().cellArray;
+
+  player1.score = 0;
+
+  player2.score = 0;
 
   let activePlayer = player1;
 
@@ -34,7 +28,7 @@ function GameController(player1 = 1, player2 = 2) {
 
   function playRound(col, row) {
     if (board.getCellValues()[col][row] === 0) {
-      board.changeCellValue(activePlayer, col, row);
+      board.changeCellValue(activePlayer.i, col, row);
     } else {
       console.log("spot taken");
       return;
@@ -46,16 +40,26 @@ function GameController(player1 = 1, player2 = 2) {
   }
 
   function switchPlayer() {
+    console.log(activePlayer.playerDisplay.querySelector(".player"));
+
+    activePlayer.playerDisplay
+      .querySelector(".player")
+      .classList.remove("current");
+
     if (activePlayer === player1) {
       activePlayer = player2;
     } else {
       activePlayer = player1;
     }
+
+    activePlayer.playerDisplay
+      .querySelector(".player")
+      .classList.add("current");
   }
 
   function newRound() {
     switchPlayer();
-    console.log(`${activePlayer}'s turn.`);
+    console.log(`${activePlayer.i}'s turn.`);
     renderBoard();
   }
 
@@ -88,19 +92,27 @@ function GameController(player1 = 1, player2 = 2) {
         boardArray[1][1] === boardArray[2][0] &&
         boardArray[1][1] !== 0)
     ) {
-      console.log(`Game over ${activePlayer} wins`);
+      result.textContent = `Game Over! ${activePlayer.i} wins!`;
+
+      activePlayer.score++;
+
       setTimeout(() => {
         resetGame();
-      }, 1000);
+      }, 1500);
     } else if (boardArray.flat().includes(0) !== true) {
-      console.log("Tie!");
+      result.textContent = `Game Over! Tie!`;
       setTimeout(() => {
         resetGame();
-      }, 1000);
+      }, 1500);
     }
   }
 
   function resetGame() {
+    result.textContent = "";
+
+    p1Counter.textContent = player1.score;
+    p2Counter.textContent = player2.score;
+
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         board.changeCellValue(0, i, j);
@@ -205,51 +217,124 @@ function Cell() {
   };
 }
 
+function DomHandler() {
+  function gameboardHandler() {
+    const cell1 = document.getElementById("cell1");
+    const cell2 = document.getElementById("cell2");
+    const cell3 = document.getElementById("cell3");
+    const cell4 = document.getElementById("cell4");
+    const cell5 = document.getElementById("cell5");
+    const cell6 = document.getElementById("cell6");
+    const cell7 = document.getElementById("cell7");
+    const cell8 = document.getElementById("cell8");
+    const cell9 = document.getElementById("cell9");
+
+    const cellArray = [
+      cell1,
+      cell2,
+      cell3,
+      cell4,
+      cell5,
+      cell6,
+      cell7,
+      cell8,
+      cell9,
+    ];
+
+    const cellBoard = document.querySelector(".gameboard");
+    return {
+      cellArray,
+      cellBoard,
+    };
+  }
+
+  function buttonHandler() {
+    const resetBtn = document.querySelector(".reset");
+
+    const submitBtn = document.querySelector(".submit-btn");
+    return {
+      resetBtn,
+      submitBtn,
+    };
+  }
+
+  function playerNameDisplay() {
+    const player1NameDisplay = document.querySelector("#player-1-name");
+    const player2NameDisplay = document.querySelector("#player-2-name");
+
+    return {
+        player1NameDisplay,
+        player2NameDisplay
+    }
+  }
+  
+
+  return {
+    gameboardHandler,
+    buttonHandler,
+    playerNameDisplay
+  };
+}
+
+DomHandler()
+  .gameboardHandler()
+  .cellBoard.addEventListener("click", (event) => {
+    let target = event.target;
+
+    if (target.id === "") {
+      target = event.target.parentNode;
+    }
+
+    console.log(target.id);
+
+    switch (target.id) {
+      case "cell1":
+        game.playRound(0, 0);
+        break;
+      case "cell2":
+        game.playRound(0, 1);
+        break;
+      case "cell3":
+        game.playRound(0, 2);
+        break;
+      case "cell4":
+        game.playRound(1, 0);
+        break;
+      case "cell5":
+        game.playRound(1, 1);
+        break;
+      case "cell6":
+        game.playRound(1, 2);
+        break;
+      case "cell7":
+        game.playRound(2, 0);
+        break;
+      case "cell8":
+        game.playRound(2, 1);
+        break;
+      case "cell9":
+        game.playRound(2, 2);
+        break;
+
+      default:
+        break;
+    }
+  });
+
+DomHandler()
+  .buttonHandler()
+  .resetBtn.addEventListener("click", () => {
+    game.resetGame();
+  });
+
+DomHandler()
+  .buttonHandler()
+  .submitBtn.addEventListener("click", () => {
+    const p1Input = document.querySelector("#player1-name");
+    const p2Input = document.querySelector("#player2-name");
+
+    DomHandler().playerNameDisplay().player1NameDisplay.textContent = p1Input.value;
+    DomHandler().playerNameDisplay().player2NameDisplay.textContent = p2Input.value;
+  });
+
 game = GameController();
-
-cellBoard.addEventListener("click", (event) => {
-  let target = event.target;
-
-  if (target.id === "") {
-    target = event.target.parentNode;
-  }
-
-  console.log(target.id);
-
-  switch (target.id) {
-    case "cell1":
-      game.playRound(0, 0);
-      break;
-    case "cell2":
-      game.playRound(0, 1);
-      break;
-    case "cell3":
-      game.playRound(0, 2);
-      break;
-    case "cell4":
-      game.playRound(1, 0);
-      break;
-    case "cell5":
-      game.playRound(1, 1);
-      break;
-    case "cell6":
-      game.playRound(1, 2);
-      break;
-    case "cell7":
-      game.playRound(2, 0);
-      break;
-    case "cell8":
-      game.playRound(2, 1);
-      break;
-    case "cell9":
-      game.playRound(2, 2);
-      break;
-
-    default:
-      break;
-  }
-});
-
-resetBtn.addEventListener("click", () => {
-  game.resetGame();
-});
