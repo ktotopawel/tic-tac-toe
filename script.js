96a17d69b3dea9edcf22124c36938e5af052f638
ktotopawel <pawel.juszczak02@gmail.com>
2025-02-07 14:57:51 +0100

    added ability to play rounds by clicking on the board

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
+const cell1 = document.getElementById("cell1");
+const cell2 = document.getElementById("cell2");
+const cell3 = document.getElementById("cell3");
+const cell4 = document.getElementById("cell4");
+const cell5 = document.getElementById("cell5");
+const cell6 = document.getElementById("cell6");
+const cell7 = document.getElementById("cell7");
+const cell8 = document.getElementById("cell8");
+const cell9 = document.getElementById("cell9");
+
+const cellBoard = document.querySelector(".gameboard");
+
 function GameController(player1 = 1, player2 = 2) {
   let board = Gameboard();
 
-  const cell1 = document.getElementById("cell1");
-  const cell2 = document.getElementById("cell2");
-  const cell3 = document.getElementById("cell3");
-  const cell4 = document.getElementById("cell4");
-  const cell5 = document.getElementById("cell5");
-  const cell6 = document.getElementById("cell6");
-  const cell7 = document.getElementById("cell7");
-  const cell8 = document.getElementById("cell8");
-  const cell9 = document.getElementById("cell9");
 
-  let cellArray = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9];
+  let cellArray = [
+    cell1,
+    cell2,
+    cell3,
+    cell4,
+    cell5,
+    cell6,
+    cell7,
+    cell8,
+    cell9,
+  ];
 
   console.log(board.getCellValues());
 
@@ -90,47 +103,47 @@ function GameController(player1 = 1, player2 = 2) {
   function resetGame() {
     board = Gameboard();
     activePlayer = player1;
+    renderBoard();
   }
 
   function renderBoard() {
     let boardStateArray = boardArray.reduce((arr, row) => {
-        arr = arr.concat(row);
-        return arr;
-    }, [])
+      arr = arr.concat(row);
+      return arr;
+    }, []);
 
     console.log(boardStateArray);
 
     for (let i = 0; i < cellArray.length; i++) {
-        if (cellArray[i].firstChild) {
-            cellArray[i].removeChild(cellArray[i].firstChild);
-        }
+      if (cellArray[i].firstChild) {
+        cellArray[i].removeChild(cellArray[i].firstChild);
+      }
     }
 
     for (let i = 0; i < boardStateArray.length; i++) {
-        switch (boardStateArray[i]) {
-            case 0: 
-                break;
-            case 1:
-                const x = document.createElement('img')
-                x.src = 'src/x.svg'
-                x.classList.add('icon');
-                cellArray[i].appendChild(x);
-                break;
-            case 2:
-                const o = document.createElement('img')
-                o.src = 'src/o.svg'
-                o.classList.add('icon');
-                cellArray[i].appendChild(o);
-                break;
-        }
-        
+      switch (boardStateArray[i]) {
+        case 0:
+          break;
+        case 1:
+          const x = document.createElement("img");
+          x.src = "src/x.svg";
+          x.classList.add("icon");
+          cellArray[i].appendChild(x);
+          break;
+        case 2:
+          const o = document.createElement("img");
+          o.src = "src/o.svg";
+          o.classList.add("icon");
+          cellArray[i].appendChild(o);
+          break;
+      }
     }
   }
 
   return {
     resetGame,
     playRound,
-    renderBoard
+    renderBoard,
   };
 }
 
@@ -194,3 +207,44 @@ game = GameController();
 game.playRound(0, 0);
 game.playRound(1, 2);
 game.playRound(0, 1);
+game.playRound(1, 1)
+
+console.log(cellBoard);
+
+
+
+cellBoard.addEventListener("click", (event) => {
+    let target = event.target;
+    switch (target.id) {
+      case "cell1":
+        game.playRound(0, 0);
+        break;
+      case "cell2":
+        game.playRound(0, 1);
+        break;
+      case "cell3":
+        game.playRound(0, 2);
+        break;
+      case "cell4":
+        game.playRound(1, 0);
+        break;
+      case "cell5":
+        game.playRound(1, 1);
+        break;
+      case "cell6":
+        game.playRound(1, 2);
+        break;
+      case "cell7":
+        game.playRound(2, 0);
+        break;
+      case "cell8":
+        game.playRound(2, 1);
+        break;
+      case "cell9":
+        game.playRound(2, 2);
+        break;
+
+      default:
+        break;
+    }
+  });
